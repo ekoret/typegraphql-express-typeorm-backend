9cf48e918205c820d5c9995d2073bc9bb21be8aa
eko-develops <ekodevelops@gmail.com>
2022-01-06 14:31:34 -0800

    Set up express server

--- server/dist/server.js
+++ server/dist/server.js
@@ -8,10 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
+const express_1 = __importDefault(require("express"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const connection = yield (0, typeorm_1.createConnection)({
         type: 'postgres',
@@ -20,6 +24,13 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         username: 'postgres',
         password: process.env.DATABASE_PASSWORD,
         entities: []
+    });
+    const app = (0, express_1.default)();
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
+    app.get('/', (req, res) => {
+        res.send('hello world');
     });
 });
 main();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import express from 'express';
 
 const main = async () => {
 
+    //TypeORM database connection to PostgreSQL
     const connection = await createConnection({
         type: 'postgres',
         host: 'localhost',
@@ -15,6 +17,17 @@ const main = async () => {
         // synchronize: true,
         entities: []
     });
+
+    //Create an express app
+    const app = express();
+    app.listen( 4000, () => {
+        console.log('server started on localhost:4000');
+    })
+    //Testing app
+    app.get('/', (req, res) => {
+        res.send('hello world');
+    });
+    
 
 }
 
