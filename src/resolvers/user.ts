a4717fc541031f06f7f960478e8322fb79acb4ce
eko-develops <ekodevelops@gmail.com>
2022-06-07 13:42:07 -0700

    Add simple mutation to create users

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,9 +1,23 @@
-import { Query, Resolver } from "type-graphql";
+import { User } from "../entities/User";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   users() {
-    return "all users";
+    return User.find();
+  }
+
+  @Mutation(() => User)
+  createUser(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string
+  ): User {
+    const newUser = User.create({
+      firstName,
+      lastName,
+    });
+
+    return newUser;
   }
 }
