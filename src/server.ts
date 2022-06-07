dca3c35ec586b615128ce2283ed17ae6cb1923cb
eko-develops <ekodevelops@gmail.com>
2022-06-07 11:43:40 -0700

    Test Apollo Server is working without connecting to database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 require("dotenv").config();
-// import "reflect-metadata";
+import "reflect-metadata";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { createConnection } from "typeorm";
+// import { createConnection } from "typeorm";
 import express from "express";
 
 //Configs
-import { typeORMconfig } from "./configs/typeORMconfig";
+// import { typeORMconfig } from "./configs/typeORMconfig";
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 
 //Resolvers
@@ -23,17 +23,17 @@ const main = async () => {
   app.use(express.json());
   app.use(cors());
 
-  //   //Create GraphQL endpoint with Apollo
-  //   const apolloServer = new ApolloServer({
-  //     schema: await buildSchema({
-  //       resolvers: [UserResolver, HelloResolver],
-  //       validate: false,
-  //     }),
-  //     context: ({ req, res }) => ({ req, res }),
-  //   });
+  //Create GraphQL endpoint with Apollo
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver, HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
 
-  //   await apolloServer.start(); //This needs to be called before applyMiddleware
-  //   apolloServer.applyMiddleware({ app }); //Applying the Apollo Server middleware to the app
+  await apolloServer.start(); //This needs to be called before applyMiddleware
+  apolloServer.applyMiddleware({ app }); //Applying the Apollo Server middleware to the app
 
   app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}`);
