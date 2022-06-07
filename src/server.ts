0fa0c5cfd342bc0cd92fd2f0c0b66240d9a47a3d
eko-develops <ekodevelops@gmail.com>
2022-06-07 12:41:32 -0700

    Update connection with try catch

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ import { createConnection } from "typeorm";
 import express from "express";
 
 //Configs
-import { typeORMconfig } from "./configs/typeORMconfig";
-const SERVER_PORT = process.env.SERVER_PORT || 4000;
+const SERVER_PORT =
+  process.env.ENVIROMENT === "DEV" ? 4000 : process.env.SERVER_PORT;
 
 //Resolvers
 import { HelloResolver } from "./resolvers/hello";
@@ -17,31 +17,31 @@ import { UserResolver } from "./resolvers/user";
 const main = async () => {
   //Make the connection to the database
   try {
-    await createConnection(typeORMconfig);
+    await createConnection(); //createConnection will look for the ormconfig.json file in the project root
+
+    //Create an express app
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+
+    //Create GraphQL endpoint with Apollo
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [UserResolver, HelloResolver],
+        validate: false,
+      }),
+      context: ({ req, res }) => ({ req, res }),
+    });
+
+    await apolloServer.start(); //This needs to be called before applyMiddleware
+    apolloServer.applyMiddleware({ app }); //Applying the Apollo Server middleware to the app
+
+    app.listen(SERVER_PORT, () => {
+      console.log(`Server listening on port ${SERVER_PORT}`);
+    });
   } catch (err) {
-    if (err) console.log(err);
+    console.log(err);
   }
-
-  //Create an express app
-  const app = express();
-  app.use(express.json());
-  app.use(cors());
-
-  //Create GraphQL endpoint with Apollo
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [UserResolver, HelloResolver],
-      validate: false,
-    }),
-    context: ({ req, res }) => ({ req, res }),
-  });
-
-  await apolloServer.start(); //This needs to be called before applyMiddleware
-  apolloServer.applyMiddleware({ app }); //Applying the Apollo Server middleware to the app
-
-  app.listen(SERVER_PORT, () => {
-    console.log(`Server listening on port ${SERVER_PORT}`);
-  });
 };
 
 main();
