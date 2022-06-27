8e463742deff8c80d80105acb8bdacb3b34a37c6
eko-develops <ekodevelops@gmail.com>
2022-06-27 10:25:21 -0700

    Add string directory back. Need to remove in future versions of TypeORM due to deprecation

--- data-source.ts
+++ data-source.ts
@@ -1,13 +1,9 @@
 import { DataSource } from 'typeorm';
-import { User } from './src/entities/User';
-
-// const DEV_ENVIROMENT = process.env.ENVIROMENT === 'DEV' ? true : false;
 
 export const sqliteDataSource = new DataSource({
   type: 'sqlite',
   database: 'testDB.db',
-  // entities: ['dist/entities/**/*.js'], //soon to be deprecated in v4.0
-  entities: [User],
+  entities: ['dist/entities/**/*.js'], //soon to be deprecated in v4.0 see: https://typeorm.io/changelog#deprecations
   synchronize: true,
   logging: false,
 });
